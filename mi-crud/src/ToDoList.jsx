c1904555fb384ef61112adda31c1b926c1dca497
themonkeypoxx <139289364+themonkeypoxx@users.noreply.github.com>
2026-06-08 14:40:10 +0000

    test1

---
+++ mi-crud/src/ToDoList.jsx
@@ -0,0 +1,11 @@
+function ToDoList(){
+    //js
+    return (
+        <>
+            <h1>Soy ToDoList</h1>
+            <h2>Tu mejor solución</h2>
+        </>
+    );
+}
+
+export default ToDoList;
